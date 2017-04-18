2443804e18b92d9b9b4c7d1786ac63aad3852c63
Francisco Perez <pacogppl@hotmail.com>
2017-04-18 22:59:56 +0200

    First Commit

---
+++ workingDir.r
@@ -0,0 +1,5 @@
+path <- "D:/Documents/MIRI/Semestre 3/Descentralized Systems/Presentacion/R Analysis"
+#path <-"E:/Documents/Mis Documentos/MIRI/Semestre 3/Descentralized Systems/Presentacion/R Analysis"
+dataDir<-glue(path,"/data")
+plotDir<-glue(path,"/plots")
+codeDir<-glue(path,"/code")
