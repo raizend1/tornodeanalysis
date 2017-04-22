9d590ae87912c2f1b5b97ccf61db9175eb67a19f
Francisco Perez <pacogppl@hotmail.com>
2017-04-22 15:12:10 +0200

    Deletion of workingDir.r

--- workingDir.r
+++
@@ -1,5 +0,0 @@
-path <- "D:/Documents/MIRI/Semestre 3/Descentralized Systems/Presentacion/R Analysis"
-#path <-"E:/Documents/Mis Documentos/MIRI/Semestre 3/Descentralized Systems/Presentacion/R Analysis"
-dataDir<-glue(path,"/data")
-plotDir<-glue(path,"/plots")
-codeDir<-glue(path,"/code")
