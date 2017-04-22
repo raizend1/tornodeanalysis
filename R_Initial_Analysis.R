499ef1173197ce2868691ae5743a05fce5255cb7
jakobgerstenlauer <jakob.gerstenlauer@gjakob.de>
2017-04-22 16:52:45 +0200

    New analysis with boosted regression trees: Which inputs can best predict if a node is malicious?

--- R_Initial_Analysis.R
+++ R_Initial_Analysis.R
@@ -12,7 +12,8 @@ setwd(dataDir)
 #Source: http://torstatus.blutmagie.de/
 tor.nodes <- read.csv("Tor_query_EXPORT.csv",header=TRUE,sep = ",",dec = ".")#,na.strings=c("","NA")
 #change all inputs of type flag to a factor
-tor.nodes[,grepl("Flag...", names(tor.nodes))] <- lapply(tor.nodes[,grepl("Flag...", names(tor.nodes))],as.factor)
+flags.indexes<-grepl("Flag...", names(tor.nodes))
+tor.nodes[,grepl("Flag...", names(tor.nodes))] <- lapply(tor.nodes[, flags.indexes],as.factor)
 dim(tor.nodes)
 #[1] 7414   25
 str(tor.nodes)
@@ -44,15 +45,116 @@ str(tor.nodes)
 # $ OrAddress         : Factor w/ 623 levels "[2001:1470:fff7:12:e0c5:f8ff:fea1:9be3]:443",..: 623 539 623 623 623 623 623 370 623 152 ...
 # ind <- apply( df1 , 1 , function(x) any( x > 0 ) )
 
-# get all malicious nodes
-tor.nodes.malicious<-subset(tor.nodes,tor.nodes$Flag...Exit==1)
-dim(tor.nodes.malicious)
+#this input should be of type numeric
+tor.nodes$ConsensusBandwidth<-as.numeric(tor.nodes$ConsensusBandwidth)
+
+#This input describes the time this host is known to the Tor network we should recode it into a numeric
+#inout describing time (here I choose months)
+tor.nodes$FirstSeen <- strptime(tor.nodes$FirstSeen,"%Y-%m-%d")
+#typeof(tor.nodes$FirstSeen)
+#newestDate<-max(tor.nodes$FirstSeen)
+oldestDate<-min(tor.nodes$FirstSeen)
+#newestDate - oldestDate 
+#Time difference of 6315.958 days
+
+#time difference in days since oldest date (I have to divide by 60*60*24 to convert from seconds to days)
+tor.nodes$FirstSeen <- as.numeric(tor.nodes$FirstSeen - oldestDate) / 86400
+summary(tor.nodes$FirstSeen)
+#Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#0    5641    6005    5864    6220    6316 
+
+# get subset with malicious nodes
+d.mal<-subset(tor.nodes,tor.nodes$Flag...Exit==1)
+dim(d.mal)
 # [1] 869  25
 
-# select features which are relevant to determine if a node is malicious
+#**************************************************************************
+# Select features which are relevant to determine if a node is malicious.
+#**************************************************************************
+
+selected.features<-vector()
+#threshold of relative variable importance
+importance.threshold<-3.0
+
+#determine which columns are numeric and which are factors
+data.type<-sapply(tor.nodes, class)
+numeric.indexes<-data.type == "integer"
+factor.indexes<-data.type == "factor"
+#numeric.indexes
+#FALSE  TRUE 
+#21     4 
+table(factor.indexes)
+#factor.indexes
+#FALSE  TRUE 
+#4    21 
+
+#First remove all variables and rows without variation
+
+#Start with numeric features:
+variances.columns<-apply(tor.nodes[numeric.indexes],2,var)
+table(variances.columns==0)
+#FALSE 
+#4 
+variances.rows<-apply(tor.nodes[numeric.indexes],1,var)
+table(variances.rows==0)
+#FALSE 
+#7414
+
+#Continue with the categorical features:
+numberOfModalities<-function(x){length(unique(x))}
+variances.columns<-apply(tor.nodes[factor.indexes],2,numberOfModalities)
+table(variances.columns)
+# variances.columns
+# 1    2   82  155  543  623 1391 1404 2026 6389 7066 7158 
+# 3    8    1    1    1    1    1    1    1    1    1    1
+#Conclusion: There are some factors with many modalities and others with very few.
+# Router.Name           Hostname         IP.Address 
+# 6389               7066               7158 
+#These three features are identifiers! So it does not make sense to use them anyway.
+#
+#Three flags have no variance they have to be excluded, too:
+sort(variances.columns)[1:3]
+# Flag...Named Flag...Running   Flag...Valid 
+# 1              1              1 
+
+#Other problematic categories are:
+# Platform            DirPort          OrAddress             ASName          FirstSeen ConsensusBandwidth 
+# 155                543                623               1391               1404               2026 
+
+selected.inputs<-names(tor.nodes)[-c(1,5,6,13,15,16,22)]
+
+table(variances.columns==0)
+#FALSE   
+#19   
+
+require("gbm")
+#fit the model
+m1.gbm <- gbm (Flag...Exit ~ . ,
+               distribution="laplace",
+               verbose=FALSE,
+               interaction.depth=4,#6
+               shrinkage=0.001,#0.001
+               n.trees = 3000,#3000
+               data=subset(tor.nodes, select = selected.inputs))
+
+#a table with variables:
+#var: feature name
+#rel.inf: relative importance measure
+ri<-summary(m1.gbm)
+# ri
+# var     rel.inf
+# DirPort                       DirPort 61.37140024
+# OrAddress                   OrAddress 20.07677804
+# Country.Code             Country.Code 13.81808553
+# Platform                     Platform  4.71609942
+# Bandwidth..KB.s.     Bandwidth..KB.s.  0.01214214
+#select data set with features having relative importance > threshold [%]
+ri<-ri[ri$rel.inf>importance.threshold,]
+selected.features<- ri$var
+
 tor.nodes.malicious.sub<- tor.nodes.malicious[,-which(names(tor.nodes.malicious) %in% c("Router.Name","OrAddress","ConsensusBandwidth","Bandwidth..KB.s.","Flag...Exit"))]
 
-# get the relantionship
+# get the relationship
 library(FactoMineR)
 tor.nodes.mca<-MCA(tor.nodes.malicious.sub,quanti.sup = c(which(names(tor.nodes.malicious.sub)%in%c("Uptime..Hours.","ASNumber","ORPort"))),level.ventil = 0.2)
 
